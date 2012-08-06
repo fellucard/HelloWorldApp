b0dd25f3bef134f3014f73f8e82ca7e6bf76a249
Mateusz Soltysik <matek92@gmail.com>
2012-08-06 10:56:40 +0200

    second edit

--- HelloWorldApp/src/HelloWorldApp.java
+++ HelloWorldApp/src/HelloWorldApp.java
@@ -1,14 +1,6 @@
-/**
- * Created with IntelliJ IDEA.
- * User: Mateusz
- * Date: 01.08.12
- * Time: 22:41
- * To change this template use File | Settings | File Templates.
- */
 
 public class HelloWorldApp {
     public static void main(String[] args) {
         System.out.println("Hello World!"); // Display the words
-        System.out.println("lol");
     }
 }
